bc76e3ba254dff44701db2907931c437fe39dc4e
Paco Brevers <francisco.brevers@tecnilogica.com>
2018-08-07 13:20:47 +0200

    Solved.

---
+++ Power of thor/episode1.py
@@ -0,0 +1,36 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+# ---
+# Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
+
+# light_x: the X position of the light of power
+# light_y: the Y position of the light of power
+# initial_tx: Thor's starting X position
+# initial_ty: Thor's starting Y position
+light_x, light_y, initial_tx, initial_ty = [int(i) for i in input().split()]
+x, y = initial_tx, initial_ty
+
+# game loop
+while True:
+    remaining_turns = int(input())  # The remaining amount of turns Thor can move. Do not remove this line.
+
+    dir = ""
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr)
+    if (light_y < y):
+        dir = "N"
+        y-=1
+    elif (light_y > y):
+        dir = "S"
+        y+=1
+    if(light_x < x):
+        dir = dir + "W"
+        x-=1
+    elif (light_x > x):
+        dir = dir + "E"
+        x+=1
+    # A single line providing the move to be made: N NE E SE S SW W or NW
+    print(dir)
